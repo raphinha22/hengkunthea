177ab2626f01a0a9240543796a8603d134d035e6
Choup Phanha <nh4kh4444@gmail.com>
2025-01-24 15:02:40 +0700

    update

--- src/components/kind/Scoopy.jsx
+++ src/components/kind/Scoopy.jsx
@@ -2,19 +2,19 @@ import moto from "../Moto";
 
 const Scoopy = () => {
   return (
-    <div className="grid grid-cols-4 gap-5 mt-[50px] px-[50px]">
+    <div className="grid md:grid-cols-4 grid-cols-2 gap-[10px] md:px-[50px] px-[20px]">
       {moto.map(({ image, text, id }) => {
         return (
           <div
             key={id}
-            className="group border border-[green] rounded-lg overflow-hidden cursor-pointer"
+            className="group border bg-white border-[green] rounded-lg overflow-hidden cursor-pointer z-[99]"
           >
             <img
               src={image}
               alt="moto"
               className="w-[300px] group-hover:scale-[1.1] duration-300 ease-in"
             />
-            <h4 className="text-center font-bold text-2xl p-5 text-maincolor font-secondfont">
+            <h4 className="text-center font-bold md:text-2xl text-xl p-5 text-maincolor font-secondfont">
               {text}
             </h4>
           </div>
