baa295c040e1d9fae74cd2a04078a7f0af3ff836
Choup Phanha <nh4kh4444@gmail.com>
2025-01-28 09:37:27 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import "./App.css";
 import MotoKind from "./components/MotoKind";
 import Action from "./components/Action";
 import Info from "./components/Info";
-import Details from "./components/Details";
 const App = () => {
   const router = createBrowserRouter([
     {
       element: <Layouts />,
       path: "/",
-      children: [ 
+      children: [
         {
           index: true,
           element: <Home />,
@@ -27,10 +26,6 @@ const App = () => {
         {
           path: "location",
           element: <Info />,
-        },
-        {
-          path: ":id",
-          element: <Details/>,
         },
       ],
     },
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,11 @@
-const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+import { useParams } from "react-router-dom";
+import all from "./all";
 
-export default Details
+const Details = () => {
+  const { id } = useParams();
+  const { text } = all.find((d) => (d.id = id));
+
+  return <div>{text}</div>;
+};
+
+export default Details;
